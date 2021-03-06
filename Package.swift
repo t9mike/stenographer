435e9daee5fa69066dbb117c58a6adc0d978153b
Michael S Muegel <mike@muegel.org>
2021-03-06 11:39:27 -0600

    Fix package description

--- Package.swift
+++ Package.swift
@@ -20,9 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Stenographer",
-            dependencies: []),
+            dependencies: [],
+            path: "Sources"),
         .testTarget(
             name: "StenographerTests",
-            dependencies: ["Stenographer"]),
+            dependencies: ["Stenographer"],
+            path: "Tests")
     ]
 )
